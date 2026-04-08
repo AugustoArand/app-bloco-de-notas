e75972ac91531c2fd05e60a4e460545fe25e30dc
Augusto Santos <augustoarandiba@gmail.com>
2026-04-07 23:39:33 -0300

    fix: correção dos middlewares de cors customizado e modificação do arquivo de routes

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,6 +1,30 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  def create
+    build_resource(sign_up_params)
+    resource.save
+
+    if resource.persisted?
+      render json: {
+        message: "Cadastro realizado com sucesso.",
+        user: {
+          id: resource.id,
+          name: resource.name,
+          email: resource.email
+        }
+      }, status: :created
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+
+      render json: {
+        message: "Erro ao criar conta.",
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
